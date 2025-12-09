8f0fa486c564b61edd4d8cdaf53dbdebe06ba8db
Kuntimaddi Adi Lakshmi <kuntimaddiadilakshmi@gmail.com>
2025-12-09 13:44:47 +0530

    Add files via upload

---
+++ qrcode.py
@@ -0,0 +1,21 @@
+import qrcode
+
+def generate_qr_code(text, file_name):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=3
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="#4B8BBE", back_color="white")
+    img.save(file_name)
+
+if name == "main":
+    text = "DataSimplifier.com" 
+    file_name = "qr_code.png"
+
+    generate_qr_code(text, file_name)
+    print(f"QR code saved as {file_name}")
